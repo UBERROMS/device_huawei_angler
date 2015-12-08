a24498ffc0924a109d72da642a16946229766c1c
beanstown106 <nbenis106@gmail.com>
2015-12-08 15:42:37 -0500

    angler: add missing drm prop
    Change-Id: I6948956f7875aafa03e294bec14ee5fb6776d23f

--- device.mk
+++ device.mk
@@ -478,6 +478,10 @@ ro.facelock.lively_timeout=2500 \
 ro.facelock.est_max_time=600 \
 ro.facelock.use_intro_anim=false
 
+# drmservice prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
 $(call inherit-product-if-exists, hardware/qcom/msm8994/msm8994.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8994/msm8994-gpu-vendor.mk)
 
