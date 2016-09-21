c7c3e6acf28eb5c1f9b311d2d469229689f0fce2
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2016-09-21 23:50:47 +0200

    Remove aosp links, this is done in the vendor now

--- aosp_angler.mk
+++ aosp_angler.mk
@@ -36,6 +36,3 @@ $(call inherit-product-if-exists, vendor/huawei/angler/device-vendor.mk)
 
 PRODUCT_PACKAGES += \
     Launcher3
-
-PRODUCT_PACKAGES += \
-    AOSPLinks
--- device.mk
+++ device.mk
@@ -255,9 +255,6 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
-PRODUCT_PACKAGES += \
-    AOSPLinks
-
 # NFC
 PRODUCT_PACKAGES += \
     libnfc-nci \
